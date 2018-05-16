a3c31e6e842e27ba49095f595e26cf5b6fccdb27
Fabian Gomez <a01209914@itesm.mx>
2018-05-16 03:50:22 -0500

    Add package arania_arco

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,5 +5,6 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs(gtest)
+subdirs(arania_arco)
 subdirs(depthimage_to_laserscan)
 subdirs(zed-ros-wrapper)
