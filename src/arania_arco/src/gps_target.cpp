58db802e4064799e028dbad771c1a04a3c7e6134
fgg <a01209914@itesm.mx>
2018-05-18 11:05:10 -0500

    add_executable for gps_target node

---
+++ src/arania_arco/src/gps_target.cpp
@@ -0,0 +1,11 @@
+ #include "ros/ros.h"
+ #include <nav2d_operator/cmd.h>
+
+  int main(int argc, char **argv){
+      //Initialize node gps_target
+      ros::init(argc, argv, "gps_target");
+
+      ros::NodeHandle n;
+
+      return 0;
+  }
