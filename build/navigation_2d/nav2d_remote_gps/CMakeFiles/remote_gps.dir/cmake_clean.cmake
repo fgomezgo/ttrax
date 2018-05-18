07915b27642bad73ca3094b16b45480d196cf23c
fgg <a01209914@itesm.mx>
2018-05-18 16:16:02 -0500

    catkin_make && Create node remote_gps.cpp

---
+++ build/navigation_2d/nav2d_remote_gps/CMakeFiles/remote_gps.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/remote_gps.dir/src/remote_gps.cpp.o"
+  "/home/fgg/catkin_ws/devel/lib/nav2d_remote_gps/remote_gps.pdb"
+  "/home/fgg/catkin_ws/devel/lib/nav2d_remote_gps/remote_gps"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/remote_gps.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/navigation_2d/nav2d_remote_gps/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/fgg/catkin_ws/src/navigation_2d/nav2d_remote_gps
+# Build directory: /home/fgg/catkin_ws/build/navigation_2d/nav2d_remote_gps
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
