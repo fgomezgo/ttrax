49cbb625bb29f1437d47cc808251c46ab5369139
fgg <a01209914@itesm.mx>
2018-05-18 23:39:02 -0500

    catkin_make

---
+++ build/navigation_2d/nav2d_remote_gps/CMakeFiles/start_mark_tf_broadcast.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/start_mark_tf_broadcast.dir/src/start_mark_tf_broadcast.cpp.o"
+  "/home/fgg/catkin_ws/devel/lib/nav2d_remote_gps/start_mark_tf_broadcast.pdb"
+  "/home/fgg/catkin_ws/devel/lib/nav2d_remote_gps/start_mark_tf_broadcast"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/start_mark_tf_broadcast.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
