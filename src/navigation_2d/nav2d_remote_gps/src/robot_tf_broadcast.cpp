3473df35dcb5c1b0c27ac105124cc82fe380cf1c
fgg <a01209914@itesm.mx>
2018-05-21 02:13:02 -0500

    Erased comments

--- src/navigation_2d/nav2d_remote_gps/src/robot_tf_broadcast.cpp
+++ src/navigation_2d/nav2d_remote_gps/src/robot_tf_broadcast.cpp
@@ -45,15 +45,6 @@ int main(int argc, char** argv){
             ROS_ERROR("%s",ex.what());
             ros::Duration(1.0).sleep();
         }
-        
-/*
-        turtlesim::Velocity vel_msg;
-        vel_msg.angular = 4.0 * atan2(transform.getOrigin().y(),
-                                    transform.getOrigin().x());
-        vel_msg.linear = 0.5 * sqrt(pow(transform.getOrigin().x(), 2) +
-                                    pow(transform.getOrigin().y(), 2));
-        turtle_vel.publish(vel_msg);
-*/
         rate.sleep();
     }
 
