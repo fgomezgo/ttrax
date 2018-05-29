8e63925bb19e6866e3628edd0ac6549dfef48690
fgg <a01209914@itesm.mx>
2018-05-29 14:47:34 -0700

    Modify code callbacks, everything runs in main

--- navigation_2d/nav2d_remote_gps/src/remote_gps.py
+++ navigation_2d/nav2d_remote_gps/src/remote_gps.py
@@ -43,20 +43,24 @@ cmd = Twist()
 #Global cmd publisher
 cmd_pub = rospy.Publisher('cmd_vel',Twist,queue_size=10)
 
-#To calculate cmd's Turn
-#Vector formed between current position and target
-toHeading = Vector(1,0)
-toTarget = Vector(5,0)
-
 #Flag to indicate thata the current target has been reached
 targetReached = False
 
 #Flag for the first time it enters the gpsDataCallback
 firstTimeInCallback = True
 
-#Init && Target
-init = GPS_Coord(0,0)
-target = GPS_Coord(0,0)
+#Flag to know if theres a target
+isTarget = False
+
+#Init && Target && Current
+init    =   GPS_Coord(0,0)
+target  =   GPS_Coord(0,0)
+current =   GPS_Coord(0,0)
+prev    =   GPS_Coord(0,0)
+
+#GPS Heading Float
+GPS_heading = 0
+Real_heading = 0
 
 #Variables for parameters
 #Get Parameters
@@ -67,95 +71,49 @@ minDistanceErrorInTarget = rospy.get_param("min_distance_error_in_target",1.0)
 minAngleToTurnInBothDirections = rospy.get_param("min_angle_to_turn_in_both_directions",2.8)
 
 def targetCallback(msg):
+    global target
+    global isTarget
     #Get GPS Target
     target.longitude    = 	msg.longitude
-    target.latitude     = 	msg.latitude 
-def gpsDataCallback(msg):
-    global firstTimeInCallback
-    global toTarget
+    target.latitude     = 	msg.latitude
+    isTarget = True
 
-    rospy.loginfo('============================ GPS DATA CB ============================')
+def gpsDataCallback(msg):
+    global current
+    global prev
+    global firstTimeInCallback
+    if msg.longitude == 0 and msg.latitude == 0:
+        return 
     if firstTimeInCallback:
-        #Add the current position
         init.longitude = msg.longitude
         init.latitude = msg.latitude
-        rospy.loginfo('Initial Position: '+str(init.longitude)+' '+str(init.latitude))
         firstTimeInCallback = False
-    #Ignore target if is set to 0
-    if target.longitude == 0 and target.latitude == 0:
-        rospy.loginfo('No new target yet defined!')
-        return
-
-    #Calculate vector between position and target
-    xGPS = target.longitude-msg.longitude
-    yGPS = target.latitude-msg.latitude
-    xGPS = (xGPS/GPS_FACTOR)*LONG_TO_M
-    yGPS = (yGPS/GPS_FACTOR)*LAT_TO_M
-
-    toTarget = Vector(xGPS,yGPS)
-
-    rospy.loginfo('Position: '+str(msg.longitude)+' '+str(msg.latitude))
-    rospy.loginfo('Target:   '+str(target.longitude)+' '+str(target.latitude))
-    rospy.loginfo('Coord:    '+str(xGPS)+' '+str(yGPS))
-
-    distance = toTarget.magnitude()
-    rospy.loginfo('Distance: '+str(distance))
-    #Threshold to slowdown velocity
-    if distance > minDistanceForMaxVel:
-        cmd.linear.x = maxVel
+        #In First one Prev = Curr
+        current.longitude    = 	msg.longitude
+        current.latitude     = 	msg.latitude
+        prev.longitude       = 	msg.longitude
+        prev.latitude        = 	msg.latitude
     else:
-        cmd.linear.x = max((maxVel/minDistanceForMaxVel)*distance,minVel)
-    cmd_pub.publish(cmd)
+        prev.longitude       = current.longitude
+        prev.latitude        = current.latitude
+        current.longitude    = 	msg.longitude
+        current.latitude     = 	msg.latitude
 
-    #End if minError is reached
-    if distance < minDistanceErrorInTarget:
-        #Stop Rover
-        cmd.linear.x = cmd.angular.z = 0.0
-        cmd_pub.publish(cmd)
-        targetReached = True
-    rospy.loginfo('Velocity: '+str(cmd.linear.x))
-    rospy.loginfo('=====================================================================')
 
 def gpsHeadingCallback(msg):
-    global toTarget
-    rospy.loginfo("============================ HEADING CB ============================")
-    rospy.loginfo("Heading:         "+str(msg.data))
-    #Tranform heading
-    heading = (msg.data*pi)/180.0 + pi/2.0
-    rospy.loginfo("Heading:         "+str(heading*180/pi))
+    global GPS_heading
+    global Real_heading
+    GPS_heading = msg.data
+    Real_heading = (GPS_heading*pi)/180.0 + pi/2.0
 
-    if target.longitude == 0 and target.latitude == 0:
-        rospy.loginfo('No new target yet defined!')
-        return
-
-    #Calculate angle between heading and toTarget
-    toHeading = Vector(cos(heading),sin(heading))
-    angle = innerAngle(toTarget,toHeading)
-    cP = crossProduct(toTarget,toHeading)
-
-    rospy.loginfo("Vector Target:   "+str(toTarget.x)+' '+str(toTarget.y))
-    rospy.loginfo("Vector Heading:  "+str(toHeading.x)+' '+str(toHeading.y))
-    
-    if angle < minAngleToTurnInBothDirections:
-        if cP > 0:
-            angle *= -1.0
-    
-    rospy.loginfo("CrossProduct:    "+str(cP))
-    rospy.loginfo("Angle Target:    "+str(atan2(toTarget.y,toTarget.x)*180/pi))
-    rospy.loginfo("Angle:           "+str(angle*180/pi))
-
-    cmd.angular.z = angle/pi
-    rospy.loginfo("Turn:            "+str(cmd.angular.z))
-
-    cmd_pub.publish(cmd)
-    rospy.loginfo("===================================================================")
 def gpsImuCallback(msg):
 	heading = msg.data
 	#rospy.loginfo("IMU Heading:     "+heading)
-
+        
 if __name__ == '__main__':
+	 
     rospy.init_node('remote_gps', anonymous=True)
-    rate = rospy.Rate(5) # 5hz
+    rate = rospy.Rate(1) # 1hz
 
     #Subscribers
     #Target Coordinates (longitude, latitude)
@@ -172,19 +130,90 @@ if __name__ == '__main__':
     cmd.angular.z = 0
     cmd.linear.x = 0
     rospy.loginfo("Waiting first callback...")
-    #rospy.spinOnce()
+
     #Wait untill first entry
     while firstTimeInCallback:
-        #rospy.spinOnce()
         pass
-    rospy.loginfo("DONE")
+    rospy.loginfo("DONE.")
 
     while not rospy.is_shutdown():
         if targetReached:
-            rospy.loginfo("Target reached!")
+            rospy.loginfo("Target reached!!!!!!!!")
             targetReached = False
-            target.longitude = target.latitude = 0.0
-        #rospy.spinOnce();	
+            target.longitude = target.latitude = 0
+        #Print Heading
+        rospy.loginfo('============================ GPS DATA ============================')
+        rospy.loginfo('Initial Position:   '+str(init.longitude)+' '+str(init.latitude))
+        if isTarget:
+            ### Linear Velocity ###
+            #Calculate vector between current and target
+            xGPS = target.longitude-current.longitude
+            yGPS = target.latitude-current.latitude
+            xGPS = (xGPS/GPS_FACTOR)*LONG_TO_M
+            yGPS = (yGPS/GPS_FACTOR)*LAT_TO_M
+            #create vector toTarget with xGPS and yGPS
+            toTarget = Vector(xGPS,yGPS)
+            #calculate distance from current to target
+            distance = toTarget.magnitude()
+            #Threshold to slowdown velocity
+            if distance > minDistanceForMaxVel:
+                cmd.linear.x = maxVel
+            else:
+                cmd.linear.x = max((maxVel/minDistanceForMaxVel)*distance,minVel)
+            cmd_pub.publish(cmd)
+            #End if minError is reached
+            if distance < minDistanceErrorInTarget:
+                #Stop Rover
+                cmd.linear.x = cmd.angular.z = 0.0
+                cmd_pub.publish(cmd)
+                targetReached = True
+                isTarget = False
+            ### Angular Velocity ###
+            #Calculate angle between heading and toTarget
+            toHeading = Vector(cos(Real_heading),sin(Real_heading))
+            angle = innerAngle(toTarget,toHeading)
+            cP = crossProduct(toTarget,toHeading)
+            #Caluculate Turn's direction based on cross product
+            if angle < minAngleToTurnInBothDirections:
+                if cP > 0:
+                    angle *= -1.0
+            #Calculate Turn
+            cmd.angular.z = angle/pi
+            cmd_pub.publish(cmd)
+            #Calculate Sim Heading
+            prevToCurr = Vector(current.longitude-prev.longitude,current.latitude-prev.latitude)
+            #flag to validate heading
+            isHeading = False
+            Simu_heading = 0
+            if prevToCurr.y != 0 or prevToCurr.x != 0:
+                Simu_heading = atan2(prevToCurr.y,prevToCurr.x)
+                isHeading = True
+            
+            #Print info
+            rospy.loginfo('Current:        '+str(current.longitude)+' '+str(current.latitude))
+            rospy.loginfo('Target:         '+str(target.longitude)+' '+str(target.latitude))
+            rospy.loginfo('Coord:          '+str(xGPS)+' '+str(yGPS))
+            rospy.loginfo('Distance:       '+str(distance))
+            rospy.loginfo('Velocity (cmd): '+str(cmd.linear.x))
+            rospy.loginfo(' ')
+            rospy.loginfo('GPS Heading:    '+str(GPS_heading))    
+            rospy.loginfo('Real Heading:   '+str(Real_heading*180/pi))
+            if isHeading:
+                rospy.loginfo('Simu Heading:   '+str(Simu_heading*180/pi))
+            else:
+                rospy.loginfo('Simu Heading:   No HEADING!')
+            rospy.loginfo('Vector Target:  '+str(toTarget.x)+' '+str(toTarget.y))
+            rospy.loginfo('Vector Heading: '+str(toHeading.x)+' '+str(toHeading.y))
+            rospy.loginfo('CrossProduct:   '+str(cP))
+            rospy.loginfo(' ')
+            rospy.loginfo('Angle Target:   '+str(atan2(toTarget.y,toTarget.x)*180/pi))
+            rospy.loginfo('Angle Heading:  '+str(Real_heading*180/pi))
+            rospy.loginfo('Angle Between:  '+str(angle*180/pi))
+            rospy.loginfo('Turn (cmd):     '+str(cmd.angular.z))
+        else:
+            rospy.loginfo('No new target yet defined!')
+        rospy.loginfo('==================================================================')
+        rate.sleep()
 
 
 
